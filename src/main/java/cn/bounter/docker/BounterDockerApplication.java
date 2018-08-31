4cc6d9d08549e93dfb8cb5616c90e8ee0757bc62
Simon-MiniPC\Simon <1317492210@qq.com>
2018-08-31 10:42:49 +0800

    初始化项目

---
+++ src/main/java/cn/bounter/docker/BounterDockerApplication.java
@@ -0,0 +1,12 @@
+package cn.bounter.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BounterDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BounterDockerApplication.class, args);
+    }
+}
---
+++ src/main/java/cn/bounter/docker/controller/DockerController.java
@@ -0,0 +1,19 @@
+package cn.bounter.docker.controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/docker")
+public class DockerController {
+
+	@GetMapping
+	public String get() {
+		return "Hello Docker!";
+	}
+
+}
